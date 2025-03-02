80ab918d35f2950869595523eea273a1792d4482
Salman <mailsbadr@gmail.com>
2025-03-02 17:55:19 -0500

    Added the gift details page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
                 {/* the final code will not pass the products to every page, but each page will call the server API */}
                 <Route path="/" element={<MainPage />} />
                 <Route path="/app" element={<MainPage />} />
-                <Route path="/app/product/:productId" element={<>Details Page</>} />
+                <Route path="/app/product/:productId" element={<DetailsPage />}/>
                 <Route path="/app/register" element={<RegisterPage/>} />
                 <Route path="/app/login" element={<LoginPage/>} />
 
